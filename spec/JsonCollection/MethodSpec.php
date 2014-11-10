c5ae1dc5b0792b5c301d703a78a77477205382b5
Mickael Vieira <contact@mickael-vieira.com>
2014-11-10 21:26:35 +0000

    Added Method Entity Class

---
+++ spec/JsonCollection/MethodSpec.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace spec\JsonCollection;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class MethodSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('JsonCollection\Method');
+    }
+
+    function it_should_extract_an_empty_array()
+    {
+        $this->toArray()->shouldBeEqualTo([]);
+    }
+
+    /**
+     * @param \JsonCollection\Option $option1
+     * @param \JsonCollection\Option $option2
+     */
+    function it_should_extract_the_options_list($option1, $option2)
+    {
+        $option1->toArray()->willReturn([
+            'value' => 'Value 1',
+            'prompt' => 'Prompt 1'
+        ]);
+        $option2->toArray()->willReturn([
+            'value' => 'Value 2',
+            'prompt' => 'Prompt 2'
+        ]);
+
+        $this->addOption($option1);
+        $this->addOption($option2);
+        $this->toArray()->shouldBeEqualTo([
+            'options' => [
+                [
+                    'value' => 'Value 1',
+                    'prompt' => 'Prompt 1'
+                ],
+                [
+                    'value' => 'Value 2',
+                    'prompt' => 'Prompt 2'
+                ]
+            ]
+        ]);
+    }
+}
