9d759cf96767fd9413ce9dc6623dace31637009f
Mickael Vieira <contact@mickael-vieira.com>
2014-11-21 15:33:26 +0000

    Refactores injection

--- src/CollectionJson/Injection.php
+++ src/CollectionJson/Injection.php
@@ -24,13 +24,14 @@ trait Injection
     public function inject(array $data)
     {
         foreach ($data as $key => $value) {
+
             $setter = "set" . $this->underscoreToCamelCase($key);
+            $adder  = "add" . ucfirst($key) . "Set";
+
             if (method_exists($this, $setter)) {
                 $this->$setter($value);
-            }
-            $setter = "add" . ucfirst($key) . "Set";
-            if (method_exists($this, $setter)) {
-                $this->$setter($value);
+            } elseif (method_exists($this, $adder)) {
+                $this->$adder($value);
             }
         }
     }
