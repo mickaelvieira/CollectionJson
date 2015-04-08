c634115be0f0b320e494a2b7b72ed17971933c4b
Mickael Vieira <contact@mickael-vieira.com>
2015-04-08 08:07:00 +0100

    Added missing license

--- src/CollectionJson/Validator/Render.php
+++ src/CollectionJson/Validator/Render.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of CollectionJson, a php implementation
+ * of the Collection+JSON Media Type
+ *
+ * (c) Mickaël Vieira <contact@mickael-vieira.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace CollectionJson\Validator;
 
 use CollectionJson\Type\Render as RenderType;
