7faa908ac1d5b955a0df6295b57e606d4fe4f4a7
Mickael Vieira <contact@mickael-vieira.com>
2017-10-17 14:39:04 +0100

    Add symfony validator as a dev dependency

--- src/CollectionJson/Validator/Dataset.php
+++ src/CollectionJson/Validator/Dataset.php
@@ -1,12 +1,21 @@
 <?php
 declare(strict_types = 1);
 
+/*
+ * This file is part of CollectionJson, a php implementation
+ * of the Collection+JSON Media Type
+ *
+ * (c) Mickaël Vieira <contact@mickael-vieira.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace CollectionJson\Validator;
 
 use CollectionJson\Entity\Data;
 
 use Symfony\Component\Validator\Validation;
-use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\ConstraintViolationInterface;
 
 /**
